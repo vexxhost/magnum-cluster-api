65bdd6810a36bfbb11a88f2cdcc063e5cfcc3cfd
okozachenko1203 <okozachenko1203@gmail.com>
2022-11-18 23:56:30 +1100

    Fix prekubeadmcommands format

--- magnum_cluster_api/resources.py
+++ magnum_cluster_api/resources.py
@@ -438,9 +438,11 @@ class KubeadmControlPlaneTemplate(Base):
                                 },
                                 # In Flatcar kubeadm configuration is in different directory because /run
                                 # can't be provisioned with ignition.
-                                "preKubeadmCommands": """
-                                - bash -c "sed -i 's/__REPLACE_NODE_NAME__/$(hostname -s)/g' /etc/kubeadm.yml"
-                                """,
+                                "preKubeadmCommands": [
+                                    """
+                                    bash -c "sed -i 's/__REPLACE_NODE_NAME__/$(hostname -s)/g' /etc/kubeadm.yml"
+                                    """
+                                ],
                                 "clusterConfiguration": {
                                     "apiServer": {
                                         "extraArgs": {
@@ -527,9 +529,11 @@ class KubeadmConfigTemplate(Base):
                             "files": [],
                             # In Flatcar kubeadm configuration is in different directory because /run
                             # can't be provisioned with ignition.
-                            "preKubeadmCommands": """
-                            - bash -c "sed -i 's/__REPLACE_NODE_NAME__/$(hostname -s)/g' /etc/kubeadm.yml"
-                            """,
+                            "preKubeadmCommands": [
+                                """
+                                bash -c "sed -i 's/__REPLACE_NODE_NAME__/$(hostname -s)/g' /etc/kubeadm.yml"
+                                """
+                            ],
                             "joinConfiguration": {
                                 "nodeRegistration": {
                                     "name": "__REPLACE_NODE_NAME__",
