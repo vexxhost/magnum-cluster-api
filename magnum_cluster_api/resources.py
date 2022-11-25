d8233ca9da430744e9713a0a8b319cc2ce42f39f
okozachenko1203 <okozachenko1203@gmail.com>
2022-11-25 21:10:40 +1100

    Land clct openstack provider override

--- magnum_cluster_api/resources.py
+++ magnum_cluster_api/resources.py
@@ -1215,6 +1215,19 @@ class ClusterClass(Base):
                                                             target: /etc/systemd/system/kubeadm.service
                                                         systemd:
                                                           units:
+                                                          - name: coreos-metadata.service
+                                                            dropins:
+                                                            - name: 20-clct-provider-override.conf
+                                                              contents: |
+                                                                [Service]
+                                                                Environment=COREOS_METADATA_OPT_PROVIDER=--provider=openstack-metadata
+                                                          - name: coreos-metadata-sshkeys@.service
+                                                            enabled: true
+                                                            dropins:
+                                                            - name: 20-clct-provider-override.conf
+                                                              contents: |
+                                                                [Service]
+                                                                Environment=COREOS_METADATA_OPT_PROVIDER=--provider=openstack-metadata
                                                           - name: kubeadm.service
                                                             dropins:
                                                             - name: 10-flatcar.conf
@@ -1290,6 +1303,19 @@ class ClusterClass(Base):
                                                             target: /etc/systemd/system/kubeadm.service
                                                         systemd:
                                                           units:
+                                                          - name: coreos-metadata.service
+                                                            dropins:
+                                                            - name: 20-clct-provider-override.conf
+                                                              contents: |
+                                                                [Service]
+                                                                Environment=COREOS_METADATA_OPT_PROVIDER=--provider=openstack-metadata
+                                                          - name: coreos-metadata-sshkeys@.service
+                                                            enabled: true
+                                                            dropins:
+                                                            - name: 20-clct-provider-override.conf
+                                                              contents: |
+                                                                [Service]
+                                                                Environment=COREOS_METADATA_OPT_PROVIDER=--provider=openstack-metadata
                                                           - name: kubeadm.service
                                                             dropins:
                                                             - name: 10-flatcar.conf
