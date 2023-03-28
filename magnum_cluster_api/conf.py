30691e0fe1b01d7dd83e85b6f4f5e9c4cf586e03
okozachenko1203 <okozachenko1203@users.noreply.github.com>
2023-03-28 18:58:32 +1100

    feat: Add manila csi
    In addition, deploy cinder-csi plugin as conditional

--- magnum_cluster_api/conf.py
+++ magnum_cluster_api/conf.py
@@ -12,7 +12,15 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
+from magnum.i18n import _
 from oslo_config import cfg
+
+auto_scaling_group = cfg.OptGroup(name="auto_scaling", title="Options for auto scaling")
+
+manila_client_group = cfg.OptGroup(
+    name="manila_client", title="Options for the Manila client"
+)
+
 
 auto_scaling_opts = [
     cfg.StrOpt(
@@ -47,5 +55,47 @@ auto_scaling_opts = [
     ),
 ]
 
+
+manila_client_opts = [
+    cfg.StrOpt(
+        "region_name",
+        help=_(
+            "Region in Identity service catalog to use for "
+            "communication with the OpenStack service."
+        ),
+    ),
+    cfg.StrOpt(
+        "endpoint_type",
+        default="publicURL",
+        help=_(
+            "Type of endpoint in Identity service catalog to use "
+            "for communication with the OpenStack service."
+        ),
+    ),
+    cfg.StrOpt(
+        "api_version",
+        default="3",
+        help=_("Version of Manila API to use in manilaclient."),
+    ),
+]
+
+common_security_opts = [
+    cfg.StrOpt("ca_file", help=_("Optional CA cert file to use in SSL connections.")),
+    cfg.StrOpt("cert_file", help=_("Optional PEM-formatted certificate chain file.")),
+    cfg.StrOpt(
+        "key_file",
+        help=_("Optional PEM-formatted file that contains the " "private key."),
+    ),
+    cfg.BoolOpt(
+        "insecure",
+        default=False,
+        help=_("If set, then the server's certificate will not " "be verified."),
+    ),
+]
+
 CONF = cfg.CONF
-CONF.register_opts(auto_scaling_opts, "auto_scaling")
+CONF.register_group(auto_scaling_group)
+CONF.register_group(manila_client_group)
+CONF.register_opts(auto_scaling_opts, group=auto_scaling_group)
+CONF.register_opts(manila_client_opts, group=manila_client_group)
+CONF.register_opts(manila_client_opts, group=common_security_opts)
