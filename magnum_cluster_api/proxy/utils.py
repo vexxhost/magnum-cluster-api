1bd6b70e0bd75f85a419c5a638aec80d334512b2
okozachenko1203 <okozachenko1203@users.noreply.github.com>
2023-10-04 00:55:11 +1100

    Fix issort lint error

--- magnum_cluster_api/proxy/utils.py
+++ magnum_cluster_api/proxy/utils.py
@@ -12,8 +12,8 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
-import os
 import fcntl
+import os
 import socket
 import struct
 from contextlib import closing
