bbccc801ae1429f82609c0466e1eaf70bcfbc3eb
Mohammed Naser <mnaser@vexxhost.com>
2023-12-22 01:30:04 -0500

    Revert "fix: Set MachineDeployment replicas as min node count when autoscale enabled"
    This breaks auto-scaling as this creates a loop where the ClusterClass
    reconciles the replicas against the auto-scaler.
    
    This reverts commit 3cc9b195d3964491983063a669af60037490dd63.

--- magnum_cluster_api/resources.py
+++ magnum_cluster_api/resources.py
@@ -1750,9 +1750,7 @@ def generate_machine_deployments_for_cluster(
             "class": "default-worker",
             "nodeVolumeDetachTimeout": CLUSTER_CLASS_NODE_VOLUME_DETACH_TIMEOUT,
             "name": ng.name,
-            "replicas": utils.get_node_group_min_node_count(ng)
-            if auto_scaling_enabled
-            else ng.node_count,
+            "replicas": None if auto_scaling_enabled else ng.node_count,
             "metadata": {
                 "annotations": {
                     AUTOSCALE_ANNOTATION_MIN: f"{utils.get_node_group_min_node_count(ng)}",  # noqa: E501
