65d7df4c208d2126139c8b1218b5a191890b4c81
okozachenko1203 <okozachenko@vexxhost.com>
2024-02-06 14:59:41 +0100

    fix typo

--- magnum_cluster_api/resources.py
+++ magnum_cluster_api/resources.py
@@ -1624,15 +1624,15 @@ class ClusterClass(Base):
                                                 "template": textwrap.dedent(
                                                     """\
                                                     "partitions":
-                                                      - "device": "/dev/vda",
-                                                        "tableType": "gpt",
-                                                        "layout": True,
-                                                        "overwrite": False,
+                                                      - "device": "/dev/vda"
+                                                        "tableType": "gpt"
+                                                        "layout": True
+                                                        "overwrite": False
                                                     "filesystems":
-                                                      - "label": "etcd_disk",
-                                                        "filesystem": "ext4",
-                                                        "device": "/dev/vda",
-                                                        "extraOpts": ["-F", "-E", "lazy_itable_init=1,lazy_journal_init=1"],
+                                                      - "label": "etcd_disk"
+                                                        "filesystem": "ext4"
+                                                        "device": "/dev/vda"
+                                                        "extraOpts": ["-F", "-E", "lazy_itable_init=1,lazy_journal_init=1"]
                                                     """
                                                 ),
                                             },
