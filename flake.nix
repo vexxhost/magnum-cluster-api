ba600636e4af42697ff159cbd7b45ebc31802e08
Mohammed Naser <mnaser@vexxhost.com>
2024-03-07 20:35:52 -0500

    chore: add flakes

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+          };
+        in
+        {
+          devShell = pkgs.mkShell
+            {
+              LD_LIBRARY_PATH = "${pkgs.stdenv.cc.cc.lib}/lib";
+
+              buildInputs = with pkgs; [
+                poetry
+              ];
+            };
+        }
+      );
+}
