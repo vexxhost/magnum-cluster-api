bf5cca00fc78cdc2981993a59502a6c503e96112
Mohammed Naser <mnaser@vexxhost.com>
2024-03-27 16:21:00 -0400

    feat: allow ovn lbs

--- magnum_cluster_api/tests/unit/test_utils.py
+++ magnum_cluster_api/tests/unit/test_utils.py
@@ -12,12 +12,13 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
+import textwrap
+
 import pykube
 import pytest
-import textwrap
 import responses
-from oslo_serialization import base64, jsonutils
 from magnum.tests.unit.objects import utils as magnum_test_utils  # type: ignore
+from oslo_serialization import base64, jsonutils
 
 from magnum_cluster_api import utils
 
@@ -44,7 +45,11 @@ class TestGenerateCloudControllerManagerConfig:
     def setup(self, context, pykube_api, mocker):
         self.context = context
         self.pykube_api = pykube_api
+
         self.cluster = magnum_test_utils.get_test_cluster(context, labels={})
+        self.cluster.cluster_template = magnum_test_utils.get_test_cluster_template(
+            self.context
+        )
 
         mock_get_openstack_api = mocker.patch(
             "magnum_cluster_api.clients.get_openstack_api"
@@ -82,11 +87,9 @@ class TestGenerateCloudControllerManagerConfig:
             },
         )
 
-    def test_generate_cloud_controller_manager_config(self, requests_mock):
+    def test_generate_cloud_controller_manager_config(self, mocker, requests_mock):
         with requests_mock as rsps:
-            rsps.add(
-                self._response_for_cloud_config_secret()
-            )
+            rsps.add(self._response_for_cloud_config_secret())
 
             config = utils.generate_cloud_controller_manager_config(
                 self.context, self.pykube_api, self.cluster
@@ -101,5 +104,55 @@ class TestGenerateCloudControllerManagerConfig:
             application-credential-secret=fake_application_credential_secret
             tls-insecure=false
 
+            [LoadBalancer]
+            lb-provider=amphora
+            """
+        )
+
+    def test_generate_cloud_controller_manager_config_for_amphora(self, requests_mock):
+        self.cluster.labels = {"octavia_provider": "amphora"}
+
+        with requests_mock as rsps:
+            rsps.add(self._response_for_cloud_config_secret())
+
+            config = utils.generate_cloud_controller_manager_config(
+                self.context, self.pykube_api, self.cluster
+            )
+
+        assert config == textwrap.dedent(
+            """\
+            [Global]
+            auth-url=http://localhost/v3
+            region=RegionOne
+            application-credential-id=fake_application_credential_id
+            application-credential-secret=fake_application_credential_secret
+            tls-insecure=false
+
+            [LoadBalancer]
+            lb-provider=amphora
+            """
+        )
+
+    def test_generate_cloud_controller_manager_config_for_ovn(self, requests_mock):
+        self.cluster.labels = {"octavia_provider": "ovn"}
+
+        with requests_mock as rsps:
+            rsps.add(self._response_for_cloud_config_secret())
+
+            config = utils.generate_cloud_controller_manager_config(
+                self.context, self.pykube_api, self.cluster
+            )
+
+        assert config == textwrap.dedent(
+            """\
+            [Global]
+            auth-url=http://localhost/v3
+            region=RegionOne
+            application-credential-id=fake_application_credential_id
+            application-credential-secret=fake_application_credential_secret
+            tls-insecure=false
+
+            [LoadBalancer]
+            lb-provider=ovn
             """
         )
