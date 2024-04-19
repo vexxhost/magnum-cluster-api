59f9ceab5fbbde67f46dc81094ed21abf10379a2
okozachenko1203 <okozachenko1203@users.noreply.github.com>
2024-04-20 05:34:33 +1000

    set global variable as true

--- magnum_cluster_api/resources.py
+++ magnum_cluster_api/resources.py
@@ -2832,3 +2832,5 @@ def set_certificate_expiry_days(
                 "certificatesExpiryDays"
             ] = 21
             utils.kube_apply_patch(kcpt)
+
+        CERTIFICATE_EXPIRY_DAY_FIX_APPLIED = True
