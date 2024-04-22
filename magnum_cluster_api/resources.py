9d6442a6e1e5fab45e43d052cde7497b17adbbae
Mohammed Naser <mnaser@vexxhost.com>
2024-04-21 21:44:37 -0400

    Add functional tests

--- magnum_cluster_api/resources.py
+++ magnum_cluster_api/resources.py
@@ -2413,10 +2413,12 @@ class Cluster(ClusterBase):
         context: context.RequestContext,
         api: pykube.HTTPClient,
         cluster: magnum_objects.Cluster,
+        cluster_class: str = CLUSTER_CLASS_NAME,
     ):
         self.context = context
         self.api = api
         self.cluster = cluster
+        self.cluster_class = cluster_class
 
     @property
     def labels(self) -> dict:
@@ -2481,7 +2483,7 @@ class Cluster(ClusterBase):
                         },
                     },
                     "topology": {
-                        "class": CLUSTER_CLASS_NAME,
+                        "class": self.cluster_class,
                         "version": utils.get_kube_tag(self.cluster),
                         "controlPlane": {
                             "metadata": {
@@ -2775,6 +2777,7 @@ def apply_cluster_from_magnum_cluster(
     context: context.RequestContext,
     api: pykube.HTTPClient,
     cluster: magnum_objects.Cluster,
+    cluster_class: str = CLUSTER_CLASS_NAME,
     skip_auto_scaling_release: bool = False,
 ) -> None:
     """
@@ -2784,7 +2787,7 @@ def apply_cluster_from_magnum_cluster(
 
     ClusterResourcesConfigMap(context, api, cluster).apply()
     ClusterResourceSet(api, cluster).apply()
-    Cluster(context, api, cluster).apply()
+    Cluster(context, api, cluster, cluster_class).apply()
 
     if not skip_auto_scaling_release and utils.get_auto_scaling_enabled(cluster):
         ClusterAutoscalerHelmRelease(api, cluster).apply()
