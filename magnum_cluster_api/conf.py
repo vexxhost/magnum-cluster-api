3a24c9fc36e738eca1a6f8583aafc6018cb68265
0x00ace <65860942+0x00ace@users.noreply.github.com>
2024-06-29 00:12:45 +0300

    Kubernetes 1.28.x support (#403)
    * k8s 1.28.x support
    
    * fix black
    
    * Switch flatcar + rockylinux to non-voting
    
    * Use capo-image-elements images
    
    We are consuming images from https://github.com/vexxhost/capo-image-elements for Ubuntu
    
    * Fix images for Cilium
    
    * Fix Rocky & co + add retries
    
    ---------
    
    Co-authored-by: Mohammed Naser <mnaser@vexxhost.com>

--- magnum_cluster_api/conf.py
+++ magnum_cluster_api/conf.py
@@ -64,6 +64,11 @@ auto_scaling_opts = [
         default="$image_repository/cluster-autoscaler:v1.27.2",
         help="Image for the cluster auto-scaler for Kubernetes v1.27.",
     ),
+    cfg.StrOpt(
+        "v1_28_image",
+        default="$image_repository/cluster-autoscaler:v1.28.5",
+        help="Image for the cluster auto-scaler for Kubernetes v1.28.",
+    ),
 ]
 
 
