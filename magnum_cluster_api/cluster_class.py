0366bab920fc723511dfe42adc1e096cf81f4dd8
Mohammed Naser <mnaser@vexxhost.com>
2024-07-01 16:58:18 -0400

    Switch to Union

--- magnum_cluster_api/cluster_class.py
+++ magnum_cluster_api/cluster_class.py
@@ -2,7 +2,7 @@
 # SPDX-License-Identifier: Apache-2.0
 
 from enum import Enum
-from typing import List, Dict, Any, Optional
+from typing import List, Dict, Any, Optional, Union
 from pydantic import BaseModel
 
 
@@ -19,13 +19,18 @@ class OpenAPIV3Schema(BaseModel):
 
     class Config:
         use_enum_values = True
-        discriminator = "type"
 
 
 class OpenAPIV3ArraySchema(OpenAPIV3Schema):
     type: OpenAPIV3SchemaType = OpenAPIV3SchemaType.ARRAY
     default: Optional[List[Any]] = None
-    items: OpenAPIV3Schema
+    items: Union[
+        "OpenAPIV3ArraySchema",
+        "OpenAPIV3BooleanSchema",
+        "OpenAPIV3IntegerSchema",
+        "OpenAPIV3ObjectSchema",
+        "OpenAPIV3StringSchema",
+    ]
 
 
 class OpenAPIV3BooleanSchema(OpenAPIV3Schema):
@@ -42,7 +47,16 @@ class OpenAPIV3ObjectSchema(OpenAPIV3Schema):
     type: OpenAPIV3SchemaType = OpenAPIV3SchemaType.OBJECT
     default: Optional[Dict[str, Any]] = None
     required: Optional[List[str]] = None
-    properties: Dict[str, OpenAPIV3Schema]
+    properties: Dict[
+        str,
+        Union[
+            "OpenAPIV3ArraySchema",
+            "OpenAPIV3BooleanSchema",
+            "OpenAPIV3IntegerSchema",
+            "OpenAPIV3ObjectSchema",
+            "OpenAPIV3StringSchema",
+        ],
+    ]
 
 
 class OpenAPIV3StringSchema(OpenAPIV3Schema):
@@ -51,12 +65,18 @@ class OpenAPIV3StringSchema(OpenAPIV3Schema):
     enum: Optional[List[str]] = None
 
 
-# Update forward references
+OpenAPIV3ArraySchema.update_forward_refs()
 OpenAPIV3ObjectSchema.update_forward_refs()
 
 
 class VariableSchema(BaseModel):
-    openAPIV3Schema: OpenAPIV3Schema
+    openAPIV3Schema: Union[
+        "OpenAPIV3ArraySchema",
+        "OpenAPIV3BooleanSchema",
+        "OpenAPIV3IntegerSchema",
+        "OpenAPIV3ObjectSchema",
+        "OpenAPIV3StringSchema",
+    ]
 
 
 class Variable(BaseModel):
