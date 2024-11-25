f376b96b9d68d9df37aa4c676b96603556e8e825
ricolin <rlin@vexxhost.com>
2024-11-25 19:55:50 +0800

    fix tempest

--- hack/run-tempest-tests.sh
+++ hack/run-tempest-tests.sh
@@ -61,9 +61,40 @@ if [[ ${UPGRADE_KUBE_TAG} != ${KUBE_TAG} ]]; then
       ${UPGRADE_IMAGE_NAME}
 fi
 
+mkdir /tmp/magnum-nodes
+
 pushd /opt/stack/tempest
+echo "Tempest configs:"
+
+cat <<EOF >> /opt/stack/tempest/etc/tempest.conf
+
+[magnum]
+flavor_id = m1.large
+master_flavor_id = m1.large
+copy_logs = true
+network_driver = ${NETWORK_DRIVER}
+image_id = ${IMAGE_OS}-kube-${KUBE_TAG}
+coe = kubernetes
+labels = '{"kube_tag": "${KUBE_TAG}", "fixed_subnet_cidr": "10.0.0.0/26"}'
+docker_storage_driver = overlay2
+
+EOF
+
+if [ ! -d /opt/stack/magnum-tempest-plugin ]; then
+    git clone https://github.com/openstack/magnum-tempest-plugin /opt/stack/magnum-tempest-plugin
+fi
+
+# install magnum-tempest-plugin
+pushd /opt/stack/magnum-tempest-plugin
+$HOME/.local/bin/pip3 install -e .
+popd
+
+echo "Run Tempest against configs:"
+cat /opt/stack/tempest/etc/tempest.conf
+
+echo "Run Tempest tests:"
 /opt/stack/data/venv/bin/tempest run -r '(^magnum_tempest_plugin)' \
-    --exclude-regex '^magnum_tempest_plugin.tests.api.v1.test_cluster.ClusterTest\.(test_create_cluster_with_zero_nodes|test_create_list_sign_delete_clusters)'
+    --exclude-regex '^magnum_tempest_plugin.tests.api.v1.test_cluster.ClusterTest.test_create_cluster_with_zero_nodes'
 popd
 
 
