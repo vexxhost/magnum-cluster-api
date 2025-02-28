d09c48ff9592fe32a3fb0b00af8399e4e10ed470
Mohammed Naser <mnaser@vexxhost.com>
2025-02-28 10:29:16 -0500

    Add pyo3-log

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,8 @@ use pyo3::{prelude::*, Bound};
 
 #[pymodule]
 fn magnum_cluster_api(m: &Bound<'_, PyModule>) -> PyResult<()> {
+    pyo3_log::init();
+
     m.add_class::<client::KubeClient>()?;
 
     Ok(())
