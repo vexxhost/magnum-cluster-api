ce867cd0fc0841c0a204d0caaeb907fca4d5e4b4
Mohammed Naser <mnaser@vexxhost.com>
2025-08-23 09:35:09 -0400

    ci: use latest k8s version (#762)
    * ci: add automation for k8s version bump
    
    Signed-off-by: Mohammed Naser <mnaser@vexxhost.com>
    
    * ci: use latest release images from capo-image-elements
    
    Signed-off-by: Mohammed Naser <mnaser@vexxhost.com>
    
    * add gh cli
    
    Signed-off-by: Mohammed Naser <mnaser@vexxhost.com>
    
    * ✨ chore: replace setup-gh action with install-gh-cli-action
    
    Signed-off-by: Mohammed Naser <mnaser@vexxhost.com>
    
    * 🔧 chore: specify gh-cli version in CI workflow
    
    Signed-off-by: Mohammed Naser <mnaser@vexxhost.com>
    
    * 🔧 chore: add GH_TOKEN environment variable for authentication
    
    Signed-off-by: Mohammed Naser <mnaser@vexxhost.com>
    
    * 🔧 chore: update jq filter for kubernetes version in CI workflow
    
    Signed-off-by: Mohammed Naser <mnaser@vexxhost.com>
    
    ---------
    
    Signed-off-by: Mohammed Naser <mnaser@vexxhost.com>

---
+++ hack/bump/kubernetes.sh
@@ -0,0 +1,36 @@
+set -e
+
+# Fetch current maintained Kubernetes versions
+VERSIONS=$(curl -s https://endoflife.date/api/v1/products/kubernetes | jq -r '.result.releases[] | select(.isMaintained == true).latest.name' | sort -V)
+
+# Build the version list for YAML
+VERSION_LINES=""
+for version in $VERSIONS; do
+    VERSION_LINES="${VERSION_LINES}          - ${version}\n"
+done
+
+# Update CI workflow in place using awk
+awk -i inplace -v versions="$VERSION_LINES" '
+/^[[:space:]]{8}kubernetes-version:[[:space:]]*$/ {
+  print
+  printf "%s", versions
+  in_k8s = 1
+  next
+}
+in_k8s && /^[[:space:]]{10}-[[:space:]]/ {
+  # Skip old version lines
+  next
+}
+in_k8s && !/^[[:space:]]{10}-[[:space:]]/ {
+  in_k8s = 0
+}
+!in_k8s {
+  print
+}
+' .github/workflows/ci.yml
+
+# Get the latest stable version (last one in the sorted list)
+LATEST_VERSION=$(echo "$VERSIONS" | tail -n 1)
+
+echo "Updated Kubernetes versions in CI workflow to:"
+echo "$VERSIONS"
